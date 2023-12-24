96536fbdc4ccf73ae4612ab483636b6d45004f4a
Mike <almazik099@gmail.com>
2023-12-24 05:15:12 +0300

    added isLoaded props, dont rdy

--- src/components/UploadForm/UploadForm.tsx
+++ src/components/UploadForm/UploadForm.tsx
@@ -1,21 +1,28 @@
 import { FC } from "react";
 import upload from './upload.png'
+import trash from './trash.png'
+import loaded from './loaded.png'
 import './style.css';
 
 
 
 
-const UploadForm: FC<{  }> = ({}) => {
+const UploadForm: FC<{ isLoaded:boolean }> = ({
+    isLoaded,
+}) => {
 
 
     return (
-      <div className="root">
-        <div className="border">
-            <img className="upload" src={upload} />
-            <span className="text">Загрузите файлы</span>
+        <div>
+            <div className="root">
+                <div className="border">
+                    <img className="upload" src={isLoaded ? loaded : upload} />
+                    <span className="text">Загрузите файлы</span>
+            </div>
+            
+            </div>
+            {isLoaded && <img className="trash" src={trash} />}
         </div>
-
-      </div>
     );
   };
 
--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,12 +1,14 @@
 import { PageLayouting } from "../../hocs";
 import { memo } from "react";
+import { UploadForm } from '../../components/UploadForm';
 
 const MainPage = () => {
   return (
     <div>
       <div>Hello user 12943710-3198</div>
+      <UploadForm isLoaded={true} />
     </div>
   );
 };
 
-export default PageLayouting(memo(MainPage), true);
+export default PageLayouting(memo(MainPage));
